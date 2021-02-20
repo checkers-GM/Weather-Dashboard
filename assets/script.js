894a842ba07cccf407f30d7ae7ab2fabd5559f66
Nathan McCaw <nathanmccaw@Analias-Mac-mini.local>
2021-02-20 16:37:34 -0500

    added logic structure to js

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+// Variables to access API and store current and previous locations
+
+var apiKey = ""
+var currentCity
+var pastCity = []
+
+// write function to get current location from cities array
+
+
+// write function to get longitude and latitude from weather api app
+
+
+// write function to handle errors
+
+
+// display previous searched locations from local storage
+
+
+// call information from weather API
+
+
