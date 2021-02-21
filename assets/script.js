af70d0296bd1e030029d3a7097dc7c6eca0f8bb4
Nathan McCaw <nathanmccaw@Analias-Mac-mini.local>
2021-02-21 16:42:13 -0500

    added basic structure for ajax get call

--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,28 @@
 // Variables to access API and store current and previous locations
 
-var apiKey = ""
-var currentCity
-var pastCity = []
+var apiKey = "bf0ae420b2bc44b42bfb5137eb028baa";
+var currentCity;
+var pastCity = [];
 
 // write function to get current location from cities array
 
-
 // write function to get longitude and latitude from weather api app
-
 
 // write function to handle errors
 
-
 // display previous searched locations from local storage
-
 
 // call information from weather API
 
-function getCurrentCity(city){
-    var queryURL = "api.openweathermap.org/data/2.5/weather?id=524901&appid=bf0ae420b2bc44b42bfb5137eb028baa"
+function getCurrentCity(city) {
+  var queryURL =
+    "api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + apiKey;
 }
 
+$.ajax;
 
 // add city to local storage city array
 
-
 // function for search button click
 
-
 // start application
-
-
